485d6de2474138438b1ffbf17195bd48577d1581
Daniel Dražić <daniel.drazic03@gmail.com>
2022-02-08 11:53:40 +0100

    Made finalMessage object from mainCollection

--- script.js
+++ script.js
@@ -18,3 +18,35 @@ const mainCollection = {
 		"Beware what you wish for. unless you have the grace to hope that your luck can be shared."
 	]
 };
+
+const getRandomIndex = (num) => {
+	return Math.floor(Math.random() * num);
+};
+
+let finalMessage = {};
+
+for (const prop in mainCollection) {
+	if (Object.hasOwnProperty.call(mainCollection, prop)) {
+		const element = mainCollection[prop];
+		const randomIndex = getRandomIndex(element.length);
+		switch (prop) {
+			case "planets":
+				if (!finalMessage.hasOwnProperty("planet")) {
+					finalMessage.planet = element[randomIndex];
+				}
+				break;
+			case "colors":
+				if (finalMessage.hasOwnProperty("color")) {
+					finalMessage.color = element[randomIndex];
+				}
+				break;
+			case "quotes":
+				if (finalMessage.hasOwnProperty("quote")) {
+					finalMessage.quote = element[randomIndex];
+				}
+				break;
+			default:
+				finalMessage.error = "Something went wrong!";
+		}
+	}
+}
