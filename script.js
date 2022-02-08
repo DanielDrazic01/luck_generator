13fb26dc097c9b7da0e366208c8ea7247c6b849f
Daniel Dražić <daniel.drazic03@gmail.com>
2022-02-08 11:35:06 +0100

    First commit

---
+++ script.js
@@ -0,0 +1,20 @@
+const mainCollection = {
+	planets: [
+		"Mercury",
+		"Venus",
+		"Earth",
+		"Mars",
+		"Jupiter",
+		"Saturn",
+		"Uranus",
+		"Neptune"
+	],
+	colors: ["Red", "Yellow", "Blue", "Orange", "Green", "Violet"],
+	quotes: [
+		"I think we consider too much the luck of the early bird and not enought the bad luck of teh early worm.",
+		"Here's the thing about luck...you don't know if it's good or bad until ou have some perspective.",
+		"Learn to recognize good luck wgen it'wavin at you, hopping to get your attention.",
+		"The harder I work, the luckier I get.",
+		"Beware what you wish for. unless you have the grace to hope that your luck can be shared."
+	]
+};
